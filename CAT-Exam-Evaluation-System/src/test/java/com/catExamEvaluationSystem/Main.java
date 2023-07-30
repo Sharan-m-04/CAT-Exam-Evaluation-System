2023e795949e3eda90e3320d59a4301dd11531a2
ExampleProjects <101189789+Sharan-m-04@users.noreply.github.com>
2023-07-30 20:18:38 +0530

    Added Excel utilities and a main class to work on

---
+++ CAT-Exam-Evaluation-System/src/test/java/com/catExamEvaluationSystem/Main.java
@@ -0,0 +1,22 @@
+package com.catExamEvaluationSystem;
+
+import java.util.Scanner;
+import utilities.ExcelUtils;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		String path = "./dataset/data.xlsx";
+		
+		//Getting data from Students sheet
+		ExcelUtils eu1 = new ExcelUtils(path, "Students");
+//		System.out.println(eu1.getRowCount());
+		for(int i = 1; i < 26; i++)
+			eu1.getCellData(i, 1);
+		
+		//Getting data from Answers sheet
+		//TODO: Below code throwing exception
+		ExcelUtils eu2 = new ExcelUtils(path, "Answers");
+		for(int j = 1; j < 11; j++)
+			eu2.getCellData(j, 1);
+	}
+}
--- CAT-Exam-Evaluation-System/src/test/java/utilities/ExcelUtils.java
+++ CAT-Exam-Evaluation-System/src/test/java/utilities/ExcelUtils.java
@@ -1,7 +1,27 @@
 package utilities;
 
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelUtils {
-	public static void main(String[] args) {
-		System.out.println("Getting data from excel file");
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	
+	public ExcelUtils(String path, String sheetName) throws Exception {
+		workbook = new XSSFWorkbook(path);
+		sheet = workbook.getSheet(sheetName);
+	}
+	public static void getCellData(int row, int col) throws IOException{
+		DataFormatter format = new DataFormatter();
+		Object value = format.formatCellValue(sheet.getRow(row).getCell(col));
+		System.out.println(value);		
+	}
+	//TODO: Row count not working
+	public static int getRowCount() throws Exception {
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		return rowCount;
 	}
 }
