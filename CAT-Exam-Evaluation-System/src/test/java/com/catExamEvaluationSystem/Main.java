56f350f5a910fd9e4d38cf907b16ed4efa7ffd9b
Srilakshmi-07 <srilakshmihm94@gmail.com>
2023-08-19 17:16:07 +0530

    commenting main function

--- CAT-Exam-Evaluation-System/src/test/java/com/catExamEvaluationSystem/Main.java
+++ CAT-Exam-Evaluation-System/src/test/java/com/catExamEvaluationSystem/Main.java
@@ -5,20 +5,20 @@ import utilities.ExcelUtils;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		int rowCount = 0;
+	/*	int rowCount = 0;
 		String path = "./CAT-Exam-Evaluation-System/dataset/data.xlsx";
 
 		//Getting data from Students sheet
 		ExcelUtils eu1 = new ExcelUtils(path, "Students");
 		rowCount = eu1.getRowCount();
 //		System.out.println(eu1.getRowCount()); //Debugging purpose
-		for(int i = 1; i < rowCount; i++)
+		for (int i = 1; i < rowCount; i++)
 			eu1.getCellData(i, 1);
-		
+
 		//Getting data from Answers sheet
 		ExcelUtils eu2 = new ExcelUtils(path, "Answers");
 		rowCount = eu2.getRowCount();
-		for(int j = 1; j < rowCount; j++)
-			eu2.getCellData(j, 1);
+		for (int j = 1; j < rowCount; j++)
+			eu2.getCellData(j, 1);*/
 	}
 }
