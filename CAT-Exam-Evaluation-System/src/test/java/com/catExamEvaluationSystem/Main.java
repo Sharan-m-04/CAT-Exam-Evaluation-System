f5cdaa69844a75f2b5ae1a42c2abc0fd65a84398
Sharan.m.04 <101189789+Sharan-m-04@users.noreply.github.com>
2023-08-27 20:13:19 +0530

    Final Draft

--- CAT-Exam-Evaluation-System/src/test/java/com/catExamEvaluationSystem/Main.java
+++ CAT-Exam-Evaluation-System/src/test/java/com/catExamEvaluationSystem/Main.java
@@ -1,70 +1,38 @@
 package com.catExamEvaluationSystem;
 
 import java.lang.String;
-import java.util.Arrays;
-import java.util.Scanner;
 import utilities.ExcelUtils;
 
 public class Main {
 	static String path = "./CAT-Exam-Evaluation-System/dataset/data.xlsx";
-	Scanner sc = new Scanner(System.in);
 	public String[] readData() throws Exception {
 		ExcelUtils eu1 = new ExcelUtils(path, "Students");
-		int sRowCount = eu1.getRowCount();
-//		int aRowCount = eu1.getRowCount();
-//		String[] names = new String[sRowCount];
+		int sRowCount = ExcelUtils.getRowCount();
 		String[] userAns = new String[sRowCount];
-		for(int i = 1; i < sRowCount; i++) {
-//			names[i] = eu1.getCellData(i, 1);
-//			System.out.println("Enter " + names[i] + "'s answers: ");
-			userAns[i] = eu1.getCellData(i, 2);
-//			System.out.println(userAns[i]);
+		for(int i = 0; i < sRowCount; i++) {
+			userAns[i] = ExcelUtils.getCellData(i, 2);
 		}
-		//For debugging
-		/*for (int j = 1; j < sRowCount; j++){
-			System.out.println(names[j] + "'s answers: " + userAns[j]);
-		}*/
 		return userAns;
 	}
-//	public static String toString(char[] a)
-//	{
-//		String string = new String(a);
-//		return string;
-//	}
 	public String keyAnswer() throws Exception {
 		ExcelUtils eu2 = new ExcelUtils(path, "Answers");
-		int aRowCount = eu2.getRowCount();
+		int aRowCount = ExcelUtils.getRowCount();
 		String[] keyAns = new String[aRowCount];
 		StringBuilder keyAnsStr = new StringBuilder();
-		for(int i = 1; i < aRowCount; i++){
-			keyAns[i] = eu2.getCellData(i, 1);
+		for(int i = 0; i < aRowCount; i++){
+			keyAns[i] = ExcelUtils.getCellData(i, 1);
 			keyAnsStr.append(keyAns[i]);
 		}
 		return keyAnsStr.toString();
 	}
-	public void calcPercentile() throws Exception {
+	public int[] calcMarks() throws Exception {
 		ExcelUtils eu1 = new ExcelUtils(path, "Students");
-		int sRowCount = eu1.getRowCount();
+		int sRowCount = ExcelUtils.getRowCount();
 		int[] sum = new int[sRowCount];
-		String[] names = new String[sRowCount];
 		String[] str1 = readData();
 		String str2 = keyAnswer();
-
-//		System.out.println(str1[0]);
-//		System.out.println(str1[1]);
-//		System.out.println(str1[25]);
-//
-//		System.out.println(str2);
-//		System.out.println(str1[1].charAt(8));
-//		System.out.println(str2.charAt(8));
-
-//		String[] str1 = {"aa--cdcba-", "dbc-cba--a", "daccbbc-b-","-acdb--bac", "bcab--d-a-","-cb-aadbcc","a--b-dc-cd","dc-cb--aab","abd-bc-cca","-cdbcaad-b","bcd-aabdc-","caa-dc-cb-","-dbac-dcba","b-a-cd-bac","abcd--bacd","-d-cadbcab","cda--b--bc","aabddbc-a-","bcb-da--ac","dc-abcdb-a","-abaccd-bd","c-ba--adbc","c--adba-cb","b-cad-a--b","dc-ba--cab"};
-
-//		String[] str2 = new String[aRowCount];
-//		str2[0] = keyAnswer();
-		for (int i = 1; i < str1.length; i++) {
+		for (int i = 0; i < str1.length; i++) {
 			for (int j = 0; j < str2.length(); j++) {
-//					System.out.println(str1[i]);
 				if (str1[i].charAt(j) == str2.charAt(j)) {
 					sum[i] += 3;
 				} else if (str1[i].charAt(j) == '-') {
@@ -74,67 +42,49 @@ public class Main {
 				}
 			}
 		}
-		int[] rank = getRank(sum);
-		//Debugging
-//		ExcelUtils eu = new ExcelUtils(path, "Students");
-//			for (int i = 1; i < sRowCount; i++) {
-//				names[i] = eu.getCellData(i, 1);
-//				System.out.println(i + "\t\t" + names[i] + "\t\t" + sum[i]);
-//			}
-		//TODO: Percentile formula: CAT Percentile= [1- (Respective Rank in CAT exam/Total no. of CAT candidates)] * 100
-		//TODO: Finally we have to display the student name {\t} and respective percentile
+		return getRank(sum);
 	}
-	public int[] getRank(int[] marks) throws Exception{
+	public int[] getRank(int[] marks) {
 		int[] ranks = new int[marks.length];
 		for(int i = 0; i < marks.length; i++){
 			int rank = 1;
-			for(int j = 0; j < marks.length; j++){
-				if (marks[j] > marks[i]) {
+			for (int mark : marks) {
+				if (mark > marks[i]) {
 					rank++;
 				}
 			}
 			ranks[i] = rank;
 		}
-		ExcelUtils eu = new ExcelUtils(path, "Students");
-		int sRowCount = eu.getRowCount();
-		String[] names = new String[sRowCount];
-		for (int i = 1; i < sRowCount; i++) {
-			names[i] = eu.getCellData(i, 1);
-//			System.out.println(i + "\t\t" + names[i] + "\t\t" + marks[i] + "\t\t" + ranks[i]);
-			System.out.println(marks[i] + "\t\t" + ranks[i]);
-		}
-//		System.out.println("\n");
-//		for(int i = 0; i < sRowCount; i++){
-//			System.out.print(ranks[i] + "\t");
-//		}
 		return ranks;
 	}
-
+	public void calcPercentile(int[] rank) throws Exception{
+		ExcelUtils eu1 = new ExcelUtils(path, "Students");
+		int sRowCount = ExcelUtils.getRowCount();
+		float[] percentile = new float[sRowCount];
+		int k = 0;
+		while(k < sRowCount){
+			percentile[k] = (1-((float) rank[k] /sRowCount)) * 100;
+			k++;
+		}
+		displayResult(percentile);
+	}
+	public void displayResult(float[] percentile) throws Exception{
+		ExcelUtils eu1 = new ExcelUtils(path, "Students");
+		int sRowCount = ExcelUtils.getRowCount();
+		String[] names = new String[sRowCount];
+		System.out.println("-----------------------");
+		System.out.printf("%-10s %-10s %n", "Name", "Percentile");
+		System.out.println("-----------------------");
+			for (int i = 0; i < sRowCount; i++) {
+				names[i] = ExcelUtils.getCellData(i, 1);
+				System.out.printf("%-10s %-10s %n", names[i], percentile[i]);
+			}
+		System.out.println("-----------------------");
+	}
 	public static void main(String[] args) throws Exception {
 		Main obj = new Main();
-//		obj.readData();
-//		obj.keyAnswer();
-		obj.calcPercentile();
-
-
-
-
-	/*	int rowCount = 0;
-		String path = "./CAT-Exam-Evaluation-System/dataset/data.xlsx";
-
-		//Getting data from Students sheet
-		ExcelUtils eu1 = new ExcelUtils(path, "Students");
-		rowCount = eu1.getRowCount();
-//		System.out.println(eu1.getRowCount()); //Debugging purpose
-		for (int i = 1; i < rowCount; i++)
-			eu1.getCellData(i, 1);
-
-		//Getting data from Answers sheet
-		ExcelUtils eu2 = new ExcelUtils(path, "Answers");
-		rowCount = eu2.getRowCount();
-		for (int j = 1; j < rowCount; j++)
-			eu2.getCellData(j, 1);*/
-
+		int[] rank = obj.calcMarks();
+		obj.calcPercentile(rank);
 	}
 }
 
--- CAT-Exam-Evaluation-System/src/test/java/utilities/ExcelUtils.java
+++ CAT-Exam-Evaluation-System/src/test/java/utilities/ExcelUtils.java
@@ -7,19 +7,23 @@ import org.apache.poi.ss.usermodel.*;
 
 public class ExcelUtils {
 	static XSSFWorkbook workbook;
+
 	static XSSFSheet sheet;
 	
 	public ExcelUtils(String path, String sheetName) throws Exception {
 		workbook = new XSSFWorkbook(path);
+
 		sheet = workbook.getSheet(sheetName);
 	}
-	public static String getCellData(int row, int col) throws Exception{
+	public static String getCellData(int row, int col) {
 		DataFormatter format = new DataFormatter();
+
 		return format.formatCellValue(sheet.getRow(row).getCell(col));
 	}
 
 	public static int getRowCount() {
 		int rowCount = 0;
+
 		for (Row row : sheet) {
 			if (row.getPhysicalNumberOfCells() > 0) {
 				rowCount++;
